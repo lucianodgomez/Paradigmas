d6d0a396e35a78c640a5d958a8eb54a7b8585eda
lucianodgomez <lucianogomez@alumno.unlam.edu.ar>
2023-09-11 15:06:37 -0300

    Update Banco.java

--- Herencia/HerenciaEjercicio1/src/edu/paradigmas/ejercicio1/Banco.java
+++ Herencia/HerenciaEjercicio1/src/edu/paradigmas/ejercicio1/Banco.java
@@ -1,5 +1,25 @@
 package edu.paradigmas.ejercicio1;
 
 public class Banco {
-
+	
+	public double saldo;
+	
+	public Banco () {
+		saldo = 0;
+	}
+	
+	public static void main(String[] args) {
+		Banco cuenta1 = new Banco ();
+		
+		System.out.println(cuenta1.saldo);
+		
+		cuenta1.saldo += 1000;
+		
+		System.out.println(cuenta1.saldo);
+		
+		cuenta1.saldo -= 550;
+		
+		System.out.println(cuenta1.saldo);
+	}
+	
 }
